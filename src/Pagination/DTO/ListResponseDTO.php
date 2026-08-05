350c245018b509d6ff9e43feb286dbf28e6099af
maxl <maxim.l@dot818.com>
2026-08-05 17:18:47 +0300

    ListResponseDTO - revert readonly class

--- src/Pagination/DTO/ListResponseDTO.php
+++ src/Pagination/DTO/ListResponseDTO.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Leads\Core\Pagination\DTO;
 
-final readonly class ListResponseDTO
+final class ListResponseDTO
 {
     public function __construct(
         public array $items,
